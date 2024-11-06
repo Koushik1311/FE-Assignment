61159e5de3464ffb89c380e54c1db4dfa5a88c1c
Koushik <programmer.koushik@gmail.com>
2024-11-06 13:52:37 +0530

    Static content for Mobile Navbar

---
+++ src/static/MobileNavbarContent.ts
@@ -0,0 +1,20 @@
+import { House, Ticket, User, Video } from "lucide-react";
+
+export const MobileNavbarContent = [
+  {
+    label: "Home",
+    icon: House,
+  },
+  {
+    label: "Movies",
+    icon: Video,
+  },
+  {
+    label: "Live Events",
+    icon: Ticket,
+  },
+  {
+    label: "Profile",
+    icon: User,
+  },
+];
