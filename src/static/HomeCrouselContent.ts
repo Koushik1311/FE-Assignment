6c88d9b02db52d9ccd10ad250f279073ba5d732b
Koushik <programmer.koushik@gmail.com>
2024-11-08 00:04:33 +0530

    static data

---
+++ src/static/HomeCrouselContent.ts
@@ -0,0 +1,5 @@
+export const HomeCrouselContent = [
+  "/home/1726036566435_playcardnewweb.avif",
+  "/home/1730303512665_manhar1240x300regions.avif",
+  "/home/1730794408406_1240x300pkkolkata.avif",
+];
--- src/static/MobileNavbarContent.ts
+++ src/static/MobileNavbarContent.ts
@@ -4,17 +4,21 @@ export const MobileNavbarContent = [
   {
     label: "Home",
     icon: House,
+    link: "/",
   },
   {
     label: "Movies",
     icon: Video,
+    link: "#",
   },
   {
     label: "Live Events",
     icon: Ticket,
+    link: "#",
   },
   {
     label: "Profile",
     icon: User,
+    link: "#",
   },
 ];
