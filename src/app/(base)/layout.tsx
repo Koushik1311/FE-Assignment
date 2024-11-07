322daca35e4262e317fdba7d061c709047b4dd1d
Koushik <programmer.koushik@gmail.com>
2024-11-08 00:01:38 +0530

    added MobileNavbar and MobileSearchbar to BaseLayout

--- src/app/(base)/layout.tsx
+++ src/app/(base)/layout.tsx
@@ -1,5 +1,13 @@
 import CategoryBar from "@/components/layouts/CategoryBar";
+import MobileNavbar from "@/components/layouts/MobileNavbar";
+import MobileSearchbar from "@/components/layouts/MobileSearchbar";
 import Navbar from "@/components/layouts/Navbar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "BookMyShow",
+  description: "Watch Movies Online in HD | Buy or Rent TV Series",
+};
 
 export default function Baselayout({
   children,
@@ -9,6 +17,12 @@ export default function Baselayout({
       <header className="container">
         {/* Navbar */}
         <Navbar />
+
+        {/* Mobile navbar show only on small device */}
+        <MobileNavbar />
+
+        {/* Mobile Searchbar show only on small device */}
+        <MobileSearchbar />
 
         {/* Category Bar */}
         <CategoryBar />
